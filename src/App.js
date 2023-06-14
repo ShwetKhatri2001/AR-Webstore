82cc4b2456fa8ff4dcc83632d21e7bbf2ef7a9b6
Vibhuti <vibhuti6378@gmail.com>
2023-06-14 15:43:06 +0530

    Error 404 page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import "@google/model-viewer/dist/model-viewer.min.js";
 import ModelViewer from "./components/ModelViewer/ModelViewer";
+import Error404 from "./components/Error404";
 
 const App = () => {
+  if(window.location.pathname === "/")
   return <ModelViewer />;
+  else return <Error404/>;
 };
 
 export default App;
