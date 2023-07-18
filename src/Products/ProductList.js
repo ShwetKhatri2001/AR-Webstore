9191a2a28d33a2dc66ff08517c7049302550dab5
Mrudula Rahate <91323146+Mrudula1205@users.noreply.github.com>
2023-07-18 21:55:56 +0400

    Add files via upload

---
+++ src/Products/ProductList.js
@@ -0,0 +1,47 @@
+import sheenchair from "../assets/models/sheenchair.glb";
+import ioschair from "../assets/models/sheenchair.usdz";
+import painting from "../assets/models/Painting.glb";
+import iospainting from "../assets/models/Painting.usdz";
+import car from "../assets/models/car.glb";
+import ioscar from "../assets/models/car.usdz";
+import car1 from "../assets/models/Car1.glb";
+import ioscar1 from "../assets/models/Car1.usdz";
+
+
+const items = [
+{
+    id: 1,
+    name: "Sheen Chair",
+    src: sheenchair,
+    iosSrc: ioschair,
+    category: "Furniture",
+    color: "Orange",
+},
+{
+    id: 2,
+    name: "Painting",
+    src: painting,
+    iosSrc: iospainting,
+    category: "Art",
+    color: "Brown",
+},
+{
+    id: 3,
+    name: "Car",
+    src: car,
+    iosSrc: ioscar,
+    category: "Vehicle",
+    color: "Blue",
+},
+
+{
+    id: 4,
+    name: "Sport Car",
+    src: car1,
+    iosSrc: ioscar1,
+    category: "Vehicle",
+    color: "Red",
+},
+
+];
+export default items;
