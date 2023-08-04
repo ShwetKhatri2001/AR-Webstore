5790b312dd8013f9d385aee000525a514524d44b
Mrudula1205 <mrudula12523@gmail.com>
2023-08-04 16:30:33 +0400

    updated code

--- src/components/ModelViewer/ModelViewer.js
+++ src/components/ModelViewer/ModelViewer.js
@@ -1,16 +1,45 @@
-import QRCode from "qrcode.react";
 import {React} from 'react';
 import '../../Products/ProductList.css'
 import items from '../../Products/ProductList'
-//import {useState} from 'react';
-
+import QRCode from "qrcode.react";
+import Help from "./Help";
+import { useRef, useState } from "react";
 const ModelViewer = () => {
+  const [display,setDisplay]=useState(false);
 
-  //const filteredModels = selectedCategory === "All" && selectedColor
-  //console.log(filteredModels);
+    //accessing product for full screen start 
+  const model= useRef()
+  function toggle() {
+    
+    if(!document.fullscreenElement){
+        model.current.requestFullscreen();
+    }
+    else if(document.exitFullscreen)document.exitFullscreen();
+  }
+        //  full screen code end
+
+  const modelViewer = {
+    backgroundColor: "#eee",
+    overflowX: "hidden",
+    posterColor: "#eee",
+    width: "100%",
+    maxWidth: 350,
+    height: 250,
+    borderRadius: 15,
+  };
+  const modelViewer1 = {
+    backgroundColor: "#eee",
+    overflowX: "hidden",
+    posterColor: "#eee",
+    width: "100%",
+    maxWidth: 250,
+    height: 200,
+    borderRadius: 15,
+    marginRight: 20,
+  };
 
   if (
-    navigator.userAgent.match(/iPhone/i ) ||
+    navigator.userAgent.match(/iPhone/i) ||
     navigator.userAgent.match(/webOS/i) ||
     navigator.userAgent.match(/Android/i) ||
     navigator.userAgent.match(/iPad/i) ||
@@ -20,73 +49,126 @@ const ModelViewer = () => {
   ) {
     return (
       <>
-        <section className="Mobile1">
-        {items.map((item)=>(
-          <div key={item.id} className="Mobile">
+        {items.map((item)=> (
+            <div class="model-view">
             <model-viewer
-              className= "model_Viewer"
+            ref={item}
+              style={modelViewer1}
               src={item.src}
               ios-src={item.iosSrc}
               alt="A 3D model of an astronaut"
               ar
               auto-rotate
               camera-controls
-              
             >
               <button slot="ar-button" className="arbutton">
                 View in your space
               </button>
+              
+              <button className="fullscreen-btn fullscreen-mob" onClick={()=>toggle()}>&#x26F6;<span>full screen</span></button>
+                
+              {
+                display?<><button className= {document.fullscreenElement?'close fz  ':'close'} onClick={()=>setDisplay(false)} >&#10006;</button>
+                <Help /></>:<button className= "help-btn help-mob"  onClick={()=>setDisplay(true)} >?<span>help</span></button>
+              }
             </model-viewer>
-
-          </div>
+            <div className="qr-sec">
+              <QRCode
+                id="1234"
+                value={window.location.href}
+                size={80}
+                bgColor={"#ffffff"}z
+                fgColor={"#000000"}
+                level={"H"}
+                includeMargin={true}
+              />
+              <div>
+                <div class="pname">Product_Name</div>
+                <div class="rating-sec">
+                <div>Rating</div>
+                  <div>
+                  <span class="star">&#9733;</span>
+                  <span class="star">&#9733;</span>
+                  <span class="star">&#9733;</span>
+                  <span>&#9733;</span>
+                  <span>&#9733;</span>
+                  </div>
+                </div>
+                <div>Rs. 1000</div>
+                <div style={{ marginTop: 20 , fontWeight:"bold" }}>
+                  Scan the QR code for AR View on mobile
+                </div>
+              </div>
+            </div>
+            <div class="add-icon add-icon-mob">
+              +
+            </div>
+            </div>
         ))}
-        </section>
         
-
-      </>  
+      </>
+      
       
     );
   } else {
     return (
       <>
-        <section className="filterSection1Style">
-        <div className="container" >
-        {items.map((item) => (
-        <div key={item.id} className="ContainerModel">
-          <h1>{item.name}</h1>
-          <model-viewer
-            className="model_Viewer"
-            src={item.src}
-            ios-src={item.iosSrc}
-            alt={`A 3D model of ${item.name}`}
-            ar
-            auto-rotate
-            camera-controls
-          ></model-viewer>
-          <div className="box1">
-            <QRCode
-              className="qrCode"
-              id={item.id}
-              value={window.location.href}
-              size={128}
-              bgColor={"#ffffff"}
-              fgColor={"#000000"}
-              level={"H"}
-              includeMargin={true}
-            />
-            <div className="Text">
-              <h5 style={{ marginTop: 30 }}>
-                Scan the QR code for AR View on mobile
-              </h5>
-              <h5>URL: {window.location.href}</h5>
+        <section className="Card">
+          {items.map((item)=>(
+            <div class="model-view" >
+              <model-viewer 
+                ref={item}
+                style={modelViewer}
+                src={item.src}
+                ios-src={item.iosSrc}
+                alt="A 3D model of an astronaut"
+                ar
+                auto-rotate
+                camera-controls
+              >
+                <button className= "fullscreen-btn" onClick={()=>toggle()}>&#x26F6;<span>full screen</span></button>
+              
+              {
+                display?<><button className={document.fullscreenElement?'close fz  ':'close'} onClick={()=>setDisplay(false)} >&#10006;</button>
+                <Help /></>:<button className="help-btn" onClick={()=>setDisplay(true)} >?<span>help</span></button>
+              }
+              </model-viewer>
+              <div className="qr-sec">
+                <QRCode
+                  id="1234"
+                  value={window.location.href}
+                  size={120}
+                  bgColor={"#ffffff"}z
+                  fgColor={"#000000"}
+                  level={"H"}
+                  includeMargin={true}
+                />
+                <div>
+                  <div class="pname">{item.name}</div>
+                  <div class="rating-sec">
+                  <div>Rating</div>
+                  <div>
+                    <span class="star">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                    <span>&#9733;</span>
+                    <span>&#9733;</span>
+                  </div>
+                  </div>
+                  <div>Rs. 1000</div>
+                  <h5 style={{ marginTop: 30 }}>
+                    Scan the QR code for AR View on mobile
+                  </h5>
+                </div>
+              </div>
+              <div class="add-icon">
+                +
+              </div>  
             </div>
-          </div>
-        </div>
-      ))}
-        </div> 
-         
-        </section>      
+          ))}
+        </section>
       </>
+      
       
     );
   }
