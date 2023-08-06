2540af06272b9a7f6786ffbc736ca8e792165ae4
Mrudula Rahate <91323146+Mrudula1205@users.noreply.github.com>
2023-08-06 14:07:38 +0400

    Responsive Product Listing Page (#103)
    * Update ModelViewer.js
    
    * Add files via upload
    
    * Update ModelViewer.css
    
    * Update ProductList.css
    
    * Update ProductList.css

--- src/components/ModelViewer/ModelViewer.js
+++ src/components/ModelViewer/ModelViewer.js
@@ -1,5 +1,6 @@
-import sheenchair from "../../assets/models/sheenchair.glb";
-import ioschair from "../../assets/models/sheenchair.usdz";
+import {React} from 'react';
+import '../../Products/ProductList.css'
+import items from '../../Products/ProductList'
 import QRCode from "qrcode.react";
 import Help from "./Help";
 import { useRef, useState } from "react";
@@ -34,6 +35,7 @@ const ModelViewer = () => {
     maxWidth: 250,
     height: 200,
     borderRadius: 15,
+    marginRight: 20,
   };
 
   if (
@@ -46,114 +48,126 @@ const ModelViewer = () => {
     navigator.userAgent.match(/Windows Phone/i)
   ) {
     return (
-      <div class="model-view">
-      <model-viewer
-      ref={model}
-        style={modelViewer1}
-        src={sheenchair}
-        ios-src={ioschair}
-        alt="A 3D model of an astronaut"
-        ar
-        auto-rotate
-        camera-controls
-      >
-        <button slot="ar-button" className="arbutton">
-          View in your space
-        </button>
-        
-        <button className="fullscreen-btn fullscreen-mob" onClick={()=>toggle()}>&#x26F6;<span>full screen</span></button>
-          
-        {
-          display?<><button className= {document.fullscreenElement?'close fz  ':'close'} onClick={()=>setDisplay(false)} >&#10006;</button>
-          <Help /></>:<button className= "help-btn help-mob"  onClick={()=>setDisplay(true)} >?<span>help</span></button>
-        }
-      </model-viewer>
-      <div className="qr-sec">
-          <QRCode
-            id="1234"
-            value={window.location.href}
-            size={80}
-            bgColor={"#ffffff"}z
-            fgColor={"#000000"}
-            level={"H"}
-            includeMargin={true}
-          />
-          <div>
-            <div class="pname">Product_Name</div>
-            <div class="rating-sec">
-            <div>Rating</div>
+      <>
+        {items.map((item)=> (
+            <div class="model-view">
+            <model-viewer
+            ref={item}
+              style={modelViewer1}
+              src={item.src}
+              ios-src={item.iosSrc}
+              alt="A 3D model of an astronaut"
+              ar
+              auto-rotate
+              camera-controls
+            >
+              <button slot="ar-button" className="arbutton">
+                View in your space
+              </button>
+              
+              <button className="fullscreen-btn fullscreen-mob" onClick={()=>toggle()}>&#x26F6;<span>full screen</span></button>
+                
+              {
+                display?<><button className= {document.fullscreenElement?'close fz  ':'close'} onClick={()=>setDisplay(false)} >&#10006;</button>
+                <Help /></>:<button className= "help-btn help-mob"  onClick={()=>setDisplay(true)} >?<span>help</span></button>
+              }
+            </model-viewer>
+            <div className="qr-sec">
+              <QRCode
+                id="1234"
+                value={window.location.href}
+                size={80}
+                bgColor={"#ffffff"}z
+                fgColor={"#000000"}
+                level={"H"}
+                includeMargin={true}
+              />
               <div>
-              <span class="star">&#9733;</span>
-              <span class="star">&#9733;</span>
-              <span class="star">&#9733;</span>
-              <span>&#9733;</span>
-              <span>&#9733;</span>
+                <div class="pname">Product_Name</div>
+                <div class="rating-sec">
+                <div>Rating</div>
+                  <div>
+                  <span class="star">&#9733;</span>
+                  <span class="star">&#9733;</span>
+                  <span class="star">&#9733;</span>
+                  <span>&#9733;</span>
+                  <span>&#9733;</span>
+                  </div>
+                </div>
+                <div>Rs. 1000</div>
+                <div style={{ marginTop: 20 , fontWeight:"bold" }}>
+                  Scan the QR code for AR View on mobile
+                </div>
               </div>
             </div>
-            <div>Rs. 1000</div>
-            <div style={{ marginTop: 20 , fontWeight:"bold" }}>
-              Scan the QR code for AR View on mobile
+            <div class="add-icon add-icon-mob">
+              +
             </div>
-          </div>
-        </div>
-        <div class="add-icon add-icon-mob">
-          +
-        </div>
-      </div>
+            </div>
+        ))}
+        
+      </>
       
     );
   } else {
     return (
-      <div class="model-view" style={{ margin: 0 }} >
-        <model-viewer 
-        ref={model}
-          style={modelViewer}
-          src={sheenchair}
-          ios-src={ioschair}
-          alt="A 3D model of an astronaut"
-          ar
-          auto-rotate
-          camera-controls
-        >
-          <button className= "fullscreen-btn" onClick={()=>toggle()}>&#x26F6;<span>full screen</span></button>
-        
-        {
-          display?<><button className={document.fullscreenElement?'close fz  ':'close'} onClick={()=>setDisplay(false)} >&#10006;</button>
-          <Help /></>:<button className="help-btn" onClick={()=>setDisplay(true)} >?<span>help</span></button>
-        }
-        </model-viewer>
-        <div className="qr-sec">
-          <QRCode
-            id="1234"
-            value={window.location.href}
-            size={120}
-            bgColor={"#ffffff"}z
-            fgColor={"#000000"}
-            level={"H"}
-            includeMargin={true}
-          />
-          <div>
-            <div class="pname">Product_Name</div>
-            <div class="rating-sec">
-            <div>Rating</div>
-              <div>
-              <span class="star">&#9733;</span>
-              <span class="star">&#9733;</span>
-              <span class="star">&#9733;</span>
-              <span>&#9733;</span>
-              <span>&#9733;</span>
+      <>
+        <section className="Card">
+          {items.map((item)=>(
+            <div class="model-view" >
+              <model-viewer 
+                ref={item}
+                style={modelViewer}
+                src={item.src}
+                ios-src={item.iosSrc}
+                alt="A 3D model of an astronaut"
+                ar
+                auto-rotate
+                camera-controls
+              >
+                <button className= "fullscreen-btn" onClick={()=>toggle()}>&#x26F6;<span>full screen</span></button>
+              
+              {
+                display?<><button className={document.fullscreenElement?'close fz  ':'close'} onClick={()=>setDisplay(false)} >&#10006;</button>
+                <Help /></>:<button className="help-btn" onClick={()=>setDisplay(true)} >?<span>help</span></button>
+              }
+              </model-viewer>
+              <div className="qr-sec">
+                <QRCode
+                  id="1234"
+                  value={window.location.href}
+                  size={120}
+                  bgColor={"#ffffff"}z
+                  fgColor={"#000000"}
+                  level={"H"}
+                  includeMargin={true}
+                />
+                <div>
+                  <div class="pname">{item.name}</div>
+                  <div class="rating-sec">
+                  <div>Rating</div>
+                  <div>
+                    <span class="star">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                    <span>&#9733;</span>
+                    <span>&#9733;</span>
+                  </div>
+                  </div>
+                  <div>Rs. 1000</div>
+                  <h5 style={{ marginTop: 30 }}>
+                    Scan the QR code for AR View on mobile
+                  </h5>
+                </div>
               </div>
+              <div class="add-icon">
+                +
+              </div>  
             </div>
-            <div>Rs. 1000</div>
-            <h5 style={{ marginTop: 30 }}>
-              Scan the QR code for AR View on mobile
-            </h5>
-          </div>
-        </div>
-        <div class="add-icon">
-          +
-        </div>  
-      </div>
+          ))}
+        </section>
+      </>    
+
     );
   }
 };
