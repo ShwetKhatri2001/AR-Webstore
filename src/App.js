b6e5621a8fbd7ff500215c86954a87c34f4b8d8e
Simran <simran28singh28@gmail.com>
2023-08-09 22:23:57 +0530

    Code Refactoring

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "@google/model-viewer/dist/model-viewer.min.js";
 import ModelViewer from "./components/ModelViewer/ModelViewer";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer.js";
+import "./App.css";
+
 const App = () => {
-  return(      
-  <BrowserRouter>
-  <Routes>
-          <Route
-            path="/"
-            element={
-                <ModelViewer />
-            }
-          />
-          <Route
-            path="*"
-            element={<ErrorPage/>}
-          />
-          </Routes>
-          <Footer/>
-  </BrowserRouter>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ModelViewer />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
