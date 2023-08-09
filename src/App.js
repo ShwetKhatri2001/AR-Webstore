45b1866d1626f2e7738f151ad01cf35ea04b1ce2
Tanuj Bordikar <91179998+tanujbordikar@users.noreply.github.com>
2023-08-09 21:51:07 +0530

    added a responsive navbar (#143)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import ModelViewer from "./components/ModelViewer/ModelViewer";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer.js";
+import Header from "./components/Header/Header"
 const App = () => {
   return(      
   <BrowserRouter>
+  <div className="app-container">
+    <Header/>
+  </div>
   <Routes>
           <Route
             path="/"
