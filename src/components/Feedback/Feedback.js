59fc6880f6f635470b43b3144dd89c009b15e135
Yuvraj <yuvrajgupta956@gmail.com>
2024-01-16 11:40:30 +0530

    Update FeedBack Page
    -> Added two more fields in the form Name and Email, so that we can find who has sent the feedback.
    ->Replaced Leave a Message with Your Feedback ✍️ Our Evolution 🚀
    -> Replaced Your Data Successfully Received with Thanks ! We've received your feedback.
    -> Added functionality to Clear the form after submission.
    -> Removed all the trash Code.

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,9 +1,11 @@
-// New Code
+/* eslint-disable no-useless-concat */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./Feedback.css";
 
 const Feedback = () => {
+  const formRef = useRef(null);
+
   useEffect(() => {
     const script1 = document.createElement("script");
     script1.src = "https://smtpjs.com/v3/smtp.js";
@@ -23,11 +25,13 @@ const Feedback = () => {
 
   const sendMail = () => {
     let likes = document.getElementById("liked").value;
+    let usersname = document.getElementById("username").value;
+    let usersemail = document.getElementById("useremail").value;
     let improvement = document.getElementById("improve").value;
     let feature = document.getElementById("features").value;
     let comment = document.getElementById("comments").value;
 
-    let body =
+    let body = "Name of the User: <br/>" + usersname + "<br/>" + "Email of the User: <br/>" + usersemail + "<br/><br/>" +
       "What did you like most about AR-Webstore? <br/>" +
       likes +
       "<br/><br/> Will our 3D and AR features improve your shopping experience if we integrate it on an online e-commerce store ?<br/>" +
@@ -43,104 +47,74 @@ const Feedback = () => {
       Host: "smtp.elasticemail.com",
       Username: "guptauv21@gmail.com",
       Password: "6B757A6226DDCFFD5B9E62BE67AAE5868267",
-      To: "shwetkhatri2001@gmail.com",
+      To: "guptauv21@gmail.com",
       From: "guptauv21@gmail.com",
       Subject: "Your Website is Reviewed",
       Body: body,
     }).then((message) => {
-      if (message === "OK") {
-        window.swal("Successfull", "Your Data Successfull Received", "success");
-      } else {
-        window.swal("Something Wrong", "Your Data is not Received", "error");
-      }
-    });
+        if (message === "OK") {
+          window.swal(
+            "Successfull",
+            "Thanks! We've received your feedback",
+            "success"
+          ).then(() => {
+            formRef.current.reset(); 
+          });
+        } else {
+          window.swal(
+            "Something Wrong",
+            "Your FeedBack is not Received",
+            "error"
+          );
+        }
+      });
   };
 
   return (
     <div className="container">
-      <h1>Leave a Message</h1>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="liked">What did you like most about AR-Webstore?</label><br/>
-        <input type="text" id="liked" placeholder="I would Like to say..." /><br/>
+      <h1>Your Feedback ✍️ Our Evolution 🚀</h1>
+      <form ref={formRef} onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="username">Name:</label><br/>
+        <input type="text" id="username" placeholder="Your Name..." />
+        <br />
 
-        <label htmlFor="improve">Will our 3D and AR features improve your shopping experience if we integrate it on an online e-commerce store ?</label><br/>
-        <input type="text" id="improve" placeholder="I would Like to say..." /><br/>
+        <label htmlFor="useremail">Email:</label><br/>
+        <input type="text" id="useremail" placeholder="Your Email..." />
+        <br />
 
-        <label htmlFor="features">What are the other features that excite you to have them on AR-Webstore ?</label><br/>
-        <input type="text" id="features" placeholder="I would Like to say..." /><br/>
+        <label htmlFor="liked">What did you like most about AR-Webstore?</label>
+        <input type="text" id="liked" placeholder="I would Like to say..." />
+        <br />
 
-        <label htmlFor="comments">Any other comments?</label><br/>
-        <textarea name="message" id="comments" placeholder="I would Like to say..." style={{ height: "200px" }}></textarea><br/>
+        <label htmlFor="improve">
+          Will our 3D and AR features improve your shopping experience if we
+          integrate it on an online e-commerce store ?
+        </label>
+        <input type="text" id="improve" placeholder="I would Like to say..." />
+        <br />
 
-        <button type="button" className = "btn" onClick={sendMail}>Send Reply</button>
+        <label htmlFor="features">
+          What are the other features that excite you to have them on
+          AR-Webstore ?
+        </label>
+        <input type="text" id="features" placeholder="I would Like to say..." />
+        <br />
+
+        <label htmlFor="comments">Any other comments?</label>
+        <br />
+        <textarea
+          name="message"
+          id="comments"
+          placeholder="I would Like to say..."
+          style={{ height: "200px" }}
+        ></textarea>
+        <br />
+        <button type="button" className="btn" onClick={sendMail}>
+          Send Reply
+        </button>
       </form>
     </div>
   );
 };
 
 export default Feedback;
-
-// Old Code
-
-// import React from "react";
-// import "./Feedback.css";
-// import { Link } from "react-router-dom";
-
-// const Feedback = () => {
-//   return (
-//     <div className="container">
-//       <h1>Thanks for exploring AR-Webstore.</h1>
-//       <h3>
-//         Share your views on the questions below at{" "}
-//         <Link
-//           to="mailto:shwetkhatri2001@gmail.com"
-//           target="_blank"
-//           className="email"
-//         >
-//           shwetkhatri2001@gmail.com
-//         </Link>
-//       </h3>
-
-//       <div>
-//         <form>
-//           <div className="form-field">
-//             <label>What did you like the most about AR-Webstore ?</label>
-//             <textarea
-//               type="text"
-//               placeholder="I would like to say ..."
-//             ></textarea>
-//           </div>
-//           <div className="form-field">
-//             <label>
-//               Will our 3D and AR features improve your shopping experience if we
-//               integrate it on an online e-commerce store ?
-//             </label>
-//             <textarea
-//               type="text"
-//               placeholder="I would like to say ..."
-//             ></textarea>
-//           </div>
-//           <div className="form-field">
-//             <label>
-//               What are the other features that excites you to have them on
-//               AR-Webstore ?
-//             </label>
-//             <textarea
-//               type="text"
-//               placeholder="I would like to say ..."
-//             ></textarea>
-//           </div>
-//           <div className="form-field">
-//             <label>Any other comments?</label>
-//             <textarea
-//               type="text"
-//               placeholder="I would like to say ..."
-//             ></textarea>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Feedback;
