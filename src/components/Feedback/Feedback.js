0b50e90b99a1050ff7cd2580a2e73ff405156bc7
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2024-01-18 01:57:48 +0530

    Feed back page (#229)
    * FeedBack Page
    
    Added the code for the new feedback page with SMTPJs Functionality
    
    * Update Feedback.js
    
    Fixed some Unintentional bug
    
    * Update FeedBack Page
    
    -> Added two more fields in the form Name and Email, so that we can find who has sent the feedback.
    ->Replaced Leave a Message with Your Feedback ✍️ Our Evolution 🚀
    -> Replaced Your Data Successfully Received with Thanks ! We've received your feedback.
    -> Added functionality to Clear the form after submission.
    -> Removed all the trash Code.
    
    * Updated SMTP configuration
    
    * Changed SMTP account

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,60 +1,118 @@
-import React from "react";
+/* eslint-disable no-useless-concat */
+
+import React, { useEffect, useRef } from "react";
 import "./Feedback.css";
-import { Link } from "react-router-dom";
 
 const Feedback = () => {
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = "https://smtpjs.com/v3/smtp.js";
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src = "https://unpkg.com/sweetalert/dist/sweetalert.min.js";
+    script2.async = true;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
+  const sendMail = () => {
+    let likes = document.getElementById("liked").value;
+    let usersname = document.getElementById("username").value;
+    let usersemail = document.getElementById("useremail").value;
+    let improvement = document.getElementById("improve").value;
+    let feature = document.getElementById("features").value;
+    let comment = document.getElementById("comments").value;
+
+    let body = "Name of the User: <br/>" + usersname + "<br/>" + "Email of the User: <br/>" + usersemail + "<br/><br/>" +
+      "What did you like most about AR-Webstore? <br/>" +
+      likes +
+      "<br/><br/> Will our 3D and AR features improve your shopping experience if we integrate it on an online e-commerce store ?<br/>" +
+      improvement +
+      "<br/><br/> What are the other features that excite you to have them on AR-Webstore ?<br/>" +
+      feature +
+      "<br/> <br/>Any other comments?<br/>" +
+      comment;
+
+    console.log(body);
+
+    window.Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "shwetkhatri2001@gmail.com",
+      Password: "BAAF238142FDFE27699F12B3FC14B1A5C9F7",
+      To: "shwetkhatri2001@gmail.com",
+      From: "shwetkhatri2001@gmail.com",
+      Subject: "AR-Webstore has got a feedback",
+      Body: body,
+    }).then((message) => {
+        if (message === "OK") {
+          window.swal(
+            "Successfull",
+            "Thanks! We've received your feedback",
+            "success"
+          ).then(() => {
+            formRef.current.reset(); 
+          });
+        } else {
+          window.swal(
+            "Something Wrong",
+            "Your FeedBack is not Received",
+            "error"
+          );
+        }
+      });
+  };
+
   return (
     <div className="container">
-      <h1>Thanks for exploring AR-Webstore.</h1>
-      <h3>
-        Share your views on the questions below at{" "}
-        <Link
-          to="mailto:shwetkhatri2001@gmail.com"
-          target="_blank"
-          className="email"
-        >
-          shwetkhatri2001@gmail.com
-        </Link>
-      </h3>
+      <h1>Your Feedback ✍️ Our Evolution 🚀</h1>
+      <form ref={formRef} onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="username">Name:</label><br/>
+        <input type="text" id="username" placeholder="Your Name..." />
+        <br />
 
-      <div>
-        <form>
-          <div className="form-field">
-            <label>What did you like the most about AR-Webstore ?</label>
-            <textarea
-              type="text"
-              placeholder="I would like to say ..."
-            ></textarea>
-          </div>
-          <div className="form-field">
-            <label>
-              Will our 3D and AR features improve your shopping experience if we
-              integrate it on an online e-commerce store ?
-            </label>
-            <textarea
-              type="text"
-              placeholder="I would like to say ..."
-            ></textarea>
-          </div>
-          <div className="form-field">
-            <label>
-              What are the other features that excites you to have them on
-              AR-Webstore ?
-            </label>
-            <textarea
-              type="text"
-              placeholder="I would like to say ..."
-            ></textarea>
-          </div>
-          <div className="form-field">
-            <label>Any other comments?</label>
-            <textarea
-              type="text"
-              placeholder="I would like to say ..."
-            ></textarea>
-          </div>
-        </form>
-      </div>
+        <label htmlFor="useremail">Email:</label><br/>
+        <input type="text" id="useremail" placeholder="Your Email..." />
+        <br />
+
+        <label htmlFor="liked">What did you like most about AR-Webstore?</label>
+        <input type="text" id="liked" placeholder="I would Like to say..." />
+        <br />
+
+        <label htmlFor="improve">
+          Will our 3D and AR features improve your shopping experience if we
+          integrate it on an online e-commerce store ?
+        </label>
+        <input type="text" id="improve" placeholder="I would Like to say..." />
+        <br />
+
+        <label htmlFor="features">
+          What are the other features that excite you to have them on
+          AR-Webstore ?
+        </label>
+        <input type="text" id="features" placeholder="I would Like to say..." />
+        <br />
+
+        <label htmlFor="comments">Any other comments?</label>
+        <br />
+        <textarea
+          name="message"
+          id="comments"
+          placeholder="I would Like to say..."
+          style={{ height: "200px" }}
+        ></textarea>
+        <br />
+        <button type="button" className="btn" onClick={sendMail}>
+          Send Reply
+        </button>
+      </form>
     </div>
   );
 };
