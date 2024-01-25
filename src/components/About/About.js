6c751816da1fcddeea2073bc2c5dd4e7fd20075d
Khushi Agarwal <141832113+KhushiAgarwal-01@users.noreply.github.com>
2024-01-25 09:28:51 +0530

    Scrollbutton changes (#241)
    * added scroll up button
    
    * scroll up button changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,6 +8,8 @@ import gun from './rightimg3.png'
 import img1 from './prob1.jpg'
 import img2 from './prob2.jpg'
 import res1 from './resource1.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope , faArrowUp} from '@fortawesome/free-solid-svg-icons'
 
 const About = () => {
     useEffect(() => {
@@ -29,6 +31,31 @@ const About = () => {
             observer.observe(section);
         });
     }, []);
+
+    const [showScrollUpButton, setShowScrollUpButton] = React.useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollY = window.scrollY;
+        const showButtonThreshold = 200;
+  
+        // Show the scroll-up button when the user scrolls down
+        setShowScrollUpButton(scrollY > showButtonThreshold);
+      };
+  
+      // Add event listener for scroll
+      window.addEventListener("scroll", handleScroll);
+  
+      // Remove event listener when the component is unmounted
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+  
+    const scrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
         <>
             <div className="about-banner">
@@ -137,6 +164,13 @@ const About = () => {
             <div><h1 className="second-head fromtop-anim" style={{ "marginTop": "13vh" }}>Contributors</h1>
                 <p className="second-head fromtop-anim" style={{ "color": "blue" }}><b>Credits go to these contributors:</b></p>
                 <ContributorSection />
+
+
+                {showScrollUpButton && (
+        <button className='scroll-up-button' onClick={scrollToTop}>
+<FontAwesomeIcon icon={faArrowUp} />
+   </button>
+      )}
             </div>
         </>
 
