c028b1c8079df98e1d4964d5e37ba44499436c9a
Girish Bari (He/Him) <38005544+Girishbari@users.noreply.github.com>
2024-02-10 22:55:36 +0530

    feat: added annotations using MODEL-VIEWER (#244)
    * feat: added annotations using MODEL-VIEWER
    
    * added requested changes

--- src/components/ModelViewer/ModelViewer.js
+++ src/components/ModelViewer/ModelViewer.js
@@ -6,9 +6,22 @@ import Help from "./Help";
 const ModelViewer = ({ item }) => {
   const [display, setDisplay] = useState(false);
   const [ARSupported, setARSupported] = useState(false);
+  const [annotate, setAnnotate] = useState(false);
+
+
+  let modelViewer1 = {
+    backgroundColor: " #ecf0f3",
+    overflowX: "hidden",
+    posterColor: "#eee",
+    width: "100%",
+    height: ARSupported ? "85%" : "75%",
+    borderRadius: 15,
+  };
 
   // Accessing product for full screen start
   const model = useRef();
+
+  console.log(item)
 
   function toggle() {
     if (!document.fullscreenElement) {
@@ -16,6 +29,13 @@ const ModelViewer = ({ item }) => {
     } else if (document.exitFullscreen) document.exitFullscreen();
   }
   // Full screen code end
+
+
+  const handleAnnotateClick = (annotation) => {
+    const { orbit, target, position } = annotation;
+    model.current.cameraTarget = position;
+    model.current.orbit = target
+  }
 
   useEffect(() => {
     if (
@@ -31,36 +51,33 @@ const ModelViewer = ({ item }) => {
     }
   }, []);
 
-  const modelViewer1 = {
-    backgroundColor: " #ecf0f3", 
-    overflowX: "hidden",
-    posterColor: "#eee",
-    width: "100%",
-    height: ARSupported ? "85%" : "75%",
-    borderRadius: 15,
-  };
+
 
   return (
     <div className="model-view">
       <model-viewer
+        key={item.id}
         ref={model}
         style={modelViewer1}
         src={item.modelSrc}
         ios-src={item.iOSSrc}
         alt="A 3D model"
         ar
-        auto-rotate
+        ar-modes="webxr scene-viewer quick-look"
         camera-controls
+        auto-rotate
+
       >
+
         {ARSupported && (
           <button slot="ar-button" className="arbutton">
             View in your space
           </button>
         )}
+
         <button className="fullscreen-btn" onClick={toggle}>
           &#x26F6;<span>full screen</span>
         </button>
-
         {display ? (
           <>
             <button
@@ -72,45 +89,69 @@ const ModelViewer = ({ item }) => {
             <Help />
           </>
         ) : (
-          <button className="help-btn" onClick={() => setDisplay(true)}>
-            ?<span>help</span>
-          </button>
-        )}
-      </model-viewer>
-      <LazyLoad>
-      {/* Card content below the model-viewer */}
-      <div className="qr-sec">
-        {!ARSupported && (
-          <QRCode
-            id={item.name}
-            value={window.location.href}
-            size={110}
-            bgColor="#ffffff"
-            fgColor="#000000"
-            level="H"
-            includeMargin
-          />
+          <>
+            <button className="help-btn" onClick={() => setDisplay(true)}>
+              ?<span>help</span>
+            </button>
+          </>
         )}
 
-        <div className="product-details">
-          <div>
-            <div className="pname">{item.name}</div>
-            <div className="rating-sec">
-              <div>Rating</div>
-              <div>
-                <span className="star">&#9733;</span>
-                <span className="star">&#9733;</span>
-                <span className="star">&#9733;</span>
-                <span>&#9733;</span>
-                <span>&#9733;</span>
+        <button className="annotate-btn" onClick={() => setAnnotate((prevState) => !prevState)}>
+          i
+        </button>
+
+        {annotate && item.annotations.map((annotate, idx) => (
+          <button
+            key={idx}
+            class="Hotspot"
+            slot={annotate.slot}
+            data-position={annotate.position}
+            data-normal={annotate.normal}
+            data-orbit={annotate.orbit}
+            data-target={annotate.target}
+            data-visibility-attribute="visible"
+            onClick={() => handleAnnotateClick(annotate)}
+          >
+            <div class="HotspotAnnotation">{annotate.title}</div>
+          </button>
+        ))}
+
+
+      </model-viewer>
+      <LazyLoad>
+        {/* Card content below the model-viewer */}
+        <div className="qr-sec">
+          {!ARSupported && (
+            <QRCode
+              id={item.name}
+              value={window.location.href}
+              size={110}
+              bgColor="#ffffff"
+              fgColor="#000000"
+              level="H"
+              includeMargin
+            />
+          )}
+
+          <div className="product-details">
+            <div>
+              <div className="pname">{item.name}</div>
+              <div className="rating-sec">
+                <div>Rating</div>
+                <div>
+                  <span className="star">&#9733;</span>
+                  <span className="star">&#9733;</span>
+                  <span className="star">&#9733;</span>
+                  <span>&#9733;</span>
+                  <span>&#9733;</span>
+                </div>
               </div>
+              <div>Rs. 1000</div>
+              {!ARSupported && <h5>Scan the QR code for AR View on mobile</h5>}
             </div>
-            <div>Rs. 1000</div>
-            {!ARSupported && <h5>Scan the QR code for AR View on mobile</h5>}
+            <div className="add-icon">+</div>
           </div>
-          <div className="add-icon">+</div>
         </div>
-      </div>
       </LazyLoad>
     </div>
   );
--- src/data/ProductItems.js
+++ src/data/ProductItems.js
@@ -15,6 +15,23 @@ const productItems = [
     iOSSrc: ioschair,
     category: "Furniture",
     color: "Orange",
+    annotations: [
+      {
+        title: "comfortable-back",
+        slot: "hotspot-1",
+        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
+        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "comfortable-seat",
+        slot: "hotspot-2",
+        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
   },
   {
     id: 2,
@@ -23,6 +40,23 @@ const productItems = [
     iOSSrc: iospainting,
     category: "Art",
     color: "Brown",
+    annotations: [
+      {
+        title: "wooden-frame",
+        slot: "hotspot-1",
+        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
+        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "pure-canvas",
+        slot: "hotspot-2",
+        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
   },
   {
     id: 3,
@@ -31,6 +65,23 @@ const productItems = [
     iOSSrc: ioscar,
     category: "Vehicle",
     color: "Blue",
+    annotations: [
+      {
+        title: "plastic-structure",
+        slot: "hotspot-1",
+        position: "-0.0036662781627494825m 0.11165170707633758m 0.07931578568217246m",
+        normal: "-0.007882343763611447m 0.9530143214961644m 0.30282267365571863m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "glossy-color",
+        slot: "hotspot-2",
+        position: "-0.09202904871903242m 0.05133736629679797m 0.0013477452825590153m",
+        normal: "-0.9976942525197127m 0.061837681663106964m 0.027969261979253417m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
   },
 
   {
@@ -40,6 +91,23 @@ const productItems = [
     iOSSrc: ioscar1,
     category: "Vehicle",
     color: "Red",
+    annotations: [
+      {
+        title: "plastic-structure",
+        slot: "hotspot-1",
+        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
+        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "glossy-color",
+        slot: "hotspot-2",
+        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
   },
 ];
 export default productItems;
