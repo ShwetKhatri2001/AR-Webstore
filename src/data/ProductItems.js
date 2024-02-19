8b6be8472dbe1b7a4b824b957cef77122c6c11af
IrfanshaikH-7 <414irfann@gmail.com>
2024-02-19 15:27:01 +0530

    feat: variant selection

--- src/data/ProductItems.js
+++ src/data/ProductItems.js
@@ -32,6 +32,31 @@ const productItems = [
         target: "-0.1053838m 0.01610652m 0.1076345m"
       },
     ]
+  }, 
+  {
+    id: 1.5,
+    name: "Sheen Chair",
+    modelSrc: sheenchair,
+    iOSSrc: ioschair,
+    category: "Furniture",
+    color: "Orange",
+    annotations: [
+      {
+        title: "comfortable-back",
+        slot: "hotspot-1",
+        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
+        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "comfortable-seat",
+        slot: "hotspot-2",
+        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
   },
   {
     id: 2,
