cd105c5ba4d9e1d48b15d2b26802ef90620a4402
Shwet Khatri <19dcs001@lnmiit.ac.in>
2024-02-24 21:45:06 +0530

    Update ProductItems.js

--- src/data/ProductItems.js
+++ src/data/ProductItems.js
@@ -37,9 +37,51 @@ const productItems = [
       },
     ]
   },
-  
   {
     id: 2,
+    name: "Office Chair",
+    modelSrc: OfficeChair,
+    iOSSrc: OfficeChairUsdz,
+    category: "Furniture",
+    color: "black",
+    annotations: [
+      {
+        title: "comfortable-back",
+        slot: "hotspot-1",
+        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
+        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "comfortable-seat",
+        slot: "hotspot-2",
+        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
+  },
+  {
+    id: 3,
+    name: "pot",
+    modelSrc: pot,
+    iOSSrc: potUsdz,
+    category: "Environment",
+    color: "Red",
+    annotations: [
+      {
+        title: "pot-structure",
+        slot: "hotspot-1",
+         position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }
+    ]
+  },
+  {
+    id: 4,
     name: "Painting",
     modelSrc: painting,
     iOSSrc: iospainting,
@@ -64,7 +106,7 @@ const productItems = [
     ]
   },
   {
-    id: 3,
+    id: 5,
     name: "Car",
     modelSrc: car,
     iOSSrc: ioscar,
@@ -88,9 +130,8 @@ const productItems = [
       },
     ]
   },
-
   {
-    id: 4,
+    id: 6,
     name: "Sport Car",
     modelSrc: car1,
     iOSSrc: ioscar1,
@@ -114,56 +155,6 @@ const productItems = [
       },
     ]
   },
-
-  {
-    id: 5,
-    name: "Office Chair",
-    modelSrc: OfficeChair,
-    iOSSrc: OfficeChairUsdz,
-    category: "Vehicle",
-    color: "black",
-    annotations: [
-      {
-        title: "Buggati - car",
-        slot: "hotspot-1",
-        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
-        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
-        orbit: "10.89188deg 119.9775deg 0.03543022m",
-        target: "-0.1053838m 0.01610652m 0.1076345m"
-      }, {
-        title: "glossy-color",
-        slot: "hotspot-2",
-        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
-        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
-        orbit: "10.89188deg 119.9775deg 0.03543022m",
-        target: "-0.1053838m 0.01610652m 0.1076345m"
-      },
-    ]
-  },
-  {
-    id: 6,
-    name: "pot",
-    modelSrc: pot,
-    iOSSrc: potUsdz,
-    category: "gaming",
-    color: "Red",
-    annotations: [
-      {
-        title: "plastic-structure",
-        slot: "hotspot-1",
-        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
-        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
-        orbit: "10.89188deg 119.9775deg 0.03543022m",
-        target: "-0.1053838m 0.01610652m 0.1076345m"
-      }, {
-        title: "glossy-color",
-        slot: "hotspot-2",
-        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
-        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
-        orbit: "10.89188deg 119.9775deg 0.03543022m",
-        target: "-0.1053838m 0.01610652m 0.1076345m"
-      },
-    ]
-  },
+  
 ];
 export default productItems;
