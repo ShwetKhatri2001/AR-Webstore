0bc18ab0652ad360ba8269de7c2d192df7dedd39
ayann07 <126085856+ayann07@users.noreply.github.com>
2024-02-27 23:31:52 +0530

    WishList Implemented

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,14 +3,13 @@ import productItems from "../../data/ProductItems";
 import ModelViewer from "../ModelViewer/ModelViewer";
 import "./ProductList.css";
 import LazyLoad from "react-lazyload";
-const ProductList = () => {
+const ProductList = ({ addToWishlist,wishlist,removeFromWishlist }) => {
+
   return (
-
     <section className="list-view">
-
-      {productItems.map((item, idx) => (
+      {productItems.map((item,idx) => (
         <LazyLoad key={idx}>
-          <ModelViewer item={item} />
+          <ModelViewer item={item} addToWishlist={addToWishlist} wishlist={wishlist} removeFromWishlist={removeFromWishlist} />
         </LazyLoad>
       ))}
     </section>
